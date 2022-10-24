146a582fcb5ac360eb15e0abc83efb9f0a3ed1e5
nobnap <m.j.d.f.r.2003@gmail.com>
2022-10-24 17:21:09 +0100

    Official DM Message changes

--- prr-app/src/prr/app/terminal/Message.java
+++ prr-app/src/prr/app/terminal/Message.java
@@ -89,7 +89,7 @@ interface Message {
          * @return report message.
          */
         static String terminalPaymentsAndDebts(String key, long payments, long debts) {
-                return "Valores para o terminal '\" + key + \"':" + payments + " (pagamentos),  " + debts + " (dívidas).";
+                return "Valores para o terminal '" + key + "':" + payments + " (pagamentos),  " + debts + " (dívidas).";
 	}
 
 	/** @return status message. */
