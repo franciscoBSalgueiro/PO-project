737e5edab678fd1b32633668efef1f73164c2d0e
nobnap <m.j.d.f.r.2003@gmail.com>
2022-11-03 00:09:32 +0000

    Added payment and debt commands

--- prr-app/src/prr/app/lookups/DoShowTerminalsWithPositiveBalance.java
+++ prr-app/src/prr/app/lookups/DoShowTerminalsWithPositiveBalance.java
@@ -15,6 +15,6 @@ class DoShowTerminalsWithPositiveBalance extends Command<Network> {
 
 	@Override
 	protected final void execute() throws CommandException {
-                //FIXME implement command
+        
 	}
 }
